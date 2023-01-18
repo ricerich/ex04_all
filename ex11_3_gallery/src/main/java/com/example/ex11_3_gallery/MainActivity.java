3020c41726c66406a9013a76119ef01ccd6f0272
ihongss <36065549+ihongss@users.noreply.github.com>
2023-01-18 09:42:03 +0900

    11장 마무리

---
+++ ex11_3_gallery/src/main/java/com/example/ex11_3_gallery/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.ex11_3_gallery;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("갤러리 영화 포스터");
+
+        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
+
+        MyGalleryAdapter galAdapter = new MyGalleryAdapter(this);
+        gallery.setAdapter(galAdapter);
+    }
+
+    public class MyGalleryAdapter extends BaseAdapter {
+
+        Context context;
+        Integer[] posterID = { R.drawable.mov11, R.drawable.mov12,
+                R.drawable.mov13, R.drawable.mov14, R.drawable.mov15,
+                R.drawable.mov16, R.drawable.mov17, R.drawable.mov18,
+                R.drawable.mov19, R.drawable.mov20 };
+
+        String[] posterTitle = { "여인의 향기", "쥬라기 공원", "포레스트 검프", "사랑의 블랙홀",
+                "혹성탈출", "아름다운비행", "내이름은 칸", "해리포터", "마더", "킹콩을 들다" };
+
+        public MyGalleryAdapter(Context c) {
+            context = c;
+        }
+
+        public int getCount() {
+            return posterID.length;
+        }
+
+        public Object getItem(int arg0) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageview = new ImageView(context);
+            imageview.setLayoutParams(new Gallery.LayoutParams(200, 300));
+            imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageview.setPadding(5, 5, 5, 5);
+            imageview.setImageResource(posterID[position]);
+
+            //final int pos = position;
+            imageview.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    ImageView ivPoster = (ImageView) findViewById(R.id.ivPoster);
+                    ivPoster.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    ivPoster.setImageResource(posterID[position]);
+
+                    //1.토스트의 2.토스트뷰의 3.텍스트뷰에 영화이름을 출력한다.
+                    Toast toast1 = new Toast(getApplicationContext());
+                    View toastView = View.inflate(getApplicationContext(),R.layout.toast1,null);
+                    TextView tv1 = toastView.findViewById(R.id.tv1);
+                    tv1.setText(posterTitle[position]);
+                    tv1.setTextColor(Color.RED);
+                    tv1.setTextSize(30);
+                    toast1.setView(toastView);
+                    toast1.show();
+
+                    return false;
+                }
+            });
+
+            return imageview;
+        }
+    }
+}
